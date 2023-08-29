64d56cd4baeebb2468c6e64d2aacaf8d207f7a0a
Trevor Gerhardt <trevorgerhardt@gmail.com>
2023-08-29 18:08:28 +0800

    Add Math Equations Plugin
    Enables inline LaTeX within Markdown files. Used the instructions here: https://docusaurus.io/docs/markdown-features/math-equations to enable.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,9 @@
 // @ts-check
 
+const math = require('remark-math')
+const katex = require('rehype-katex')
+const { EnumChangefreq } = require('sitemap')
+
 /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 const themeConfig = {
   image: 'img/android-chrome-512x512.png', // meta image for social media
@@ -54,6 +58,35 @@ if (process.env.ALGOLIA_API_KEY) {
   }
 }
 
+/** @type {import('@docusaurus/preset-classic').Options} */
+const presetClassicOptions = {
+  docs: {
+    breadcrumbs: false,
+    routeBasePath: '/',
+    sidebarPath: require.resolve('./sidebars.js'),
+    editUrl: 'https://github.com/conveyal/docs/edit/master',
+    remarkPlugins: [math],
+    rehypePlugins: [katex]
+  },
+  blog: {
+    path: './changelog',
+    routeBasePath: '/changelog',
+    blogTitle: 'Changelog',
+    blogDescription: 'Updates to Conveyal Analysis',
+    blogSidebarTitle: 'Latest changes',
+    blogSidebarCount: 1,
+    postsPerPage: 3,
+    showReadingTime: false
+  },
+  theme: {
+    customCss: require.resolve('./custom.css')
+  },
+  sitemap: { // for crawlers
+    changefreq: EnumChangefreq.WEEKLY,
+    priority: 0.5
+  }
+}
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Conveyal User Manual',
@@ -66,42 +99,19 @@ const config = {
   projectName: 'docs', 
   themeConfig,
   trailingSlash: false,
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          breadcrumbs: false,
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/conveyal/docs/edit/master'
-        },
-        blog: {
-          path: './changelog',
-          routeBasePath: '/changelog',
-          blogTitle: 'Changelog',
-          blogDescription: 'Updates to Conveyal Analysis',
-          blogSidebarTitle: 'Latest changes',
-          blogSidebarCount: 1,
-          postsPerPage: 3,
-          showReadingTime: false
-        },
-        theme: {
-          customCss: require.resolve('./custom.css'),
-        },
-        sitemap: { // for crawlers
-          changefreq: 'weekly',
-          priority: 0.5
-        }
-      }
-    ]
-  ],
+  presets: [['@docusaurus/preset-classic', presetClassicOptions]],
   scripts: [{
     src: '/js/external/script.js', 
     async: true, 
     defer: true, 
     'data-api': '/api/external/event',
     'data-domain': 'docs.conveyal.com'
+  }],
+  stylesheets: [{
+    href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+    type: 'text/css',
+    integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+    crossorigin: 'anonymous',
   }]
 }
 
